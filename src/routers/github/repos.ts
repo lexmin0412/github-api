c436e5bd29c424038901adc06ee604a6c382b768
lexmin0412 <zhangle_media@hotmail.com>
2022-02-20 20:01:20 +0800

    feat: 新增仓库基本信息接口

--- src/routers/github/repos.ts
+++ src/routers/github/repos.ts
@@ -6,13 +6,36 @@ import {
 } from '../../../config/prod'
 
 /**
+ * 获取仓库基本信息
+ */
+repos.get('/:owner/:repo', async(req, res)=>{
+	const {owner, repo } = req.params
+	console.log('userName', owner)
+	const rq = createGithubRequest({
+		uri: `${GITHUB_API_BASE}/repos/${owner}/${repo}`,
+	})
+	rq().then((resp)=>{
+		res.send({
+			code: 0,
+			data: resp,
+			msg: ''
+		})
+	})
+})
+
+/**
  * 获取仓库相关信息
  */
 repos.get('/:owner/:repo/:info_type', async(req, res)=>{
 	const { owner, repo, info_type } = req.params
+	const headers = {}
+	if ( info_type === 'readme' ) {
+		headers['Accept'] = 'application/vnd.github.v3+json'
+	}
 	const rq = createGithubRequest({
 		uri: `${GITHUB_API_BASE}/repos/${owner}/${repo}/${info_type}`,
-		qs: req.query
+		qs: req.query,
+		Headers: headers
 	})
 	rq().then((resp)=>{
 		res.send({
