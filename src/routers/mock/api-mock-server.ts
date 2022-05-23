7cb230fc58efe6283fb43b5fef3dc7d612a185ea
lexmin0412 <zhangle_dev@outlook.com>
2022-05-24 00:15:38 +0800

    feat:新增更新接口

--- src/routers/mock/api-mock-server.ts
+++ src/routers/mock/api-mock-server.ts
@@ -121,25 +121,27 @@ apiMock.get('/list', async (req: any, res: any) => {
   })
 })
 
-apiMock.get('/prod-api/mock_api_info', async (req: any, res: any) => {
+apiMock.get('/', async (req: any, res: any) => {
 
   const result: any = fs.readFileSync(databasePath, 'utf-8')
   
 
   console.log('result', result)
+
+  const list = JSON.parse(result).list
   
   res.send({
     code: 0,
-    count: result.length,
-    results: JSON.parse(result).list.sort((a: any, b: any) =>b.id-a.id),
+    count: list.length,
+    results: list.sort((a: any, b: any) =>b.id-a.id),
     msg: '恭喜我黄拿到数据啦'
   })
 } )
 
-apiMock.get('/prod-api/mock_api_detail', async (req: any, res: any) => {
+apiMock.get('/:id', async (req: any, res: any) => {
 
   const result: any = fs.readFileSync(databasePath, 'utf-8')
-  const { id } = req.query
+  const { id } = req.params
 
   const current = JSON.parse(result).list.find((item: any) => item.id === Number(id))
 
@@ -155,13 +157,14 @@ apiMock.get('/prod-api/mock_api_detail', async (req: any, res: any) => {
   })
 })
 
-apiMock.post('/prod-api/mock_api_create', async(req: {
+apiMock.post('/', async(req: {
   body: {
     describe: string
     method: string
     url: string
-    statusCode: number
-    responseBody: any
+    exception_status: number
+    normal_response: any
+    status: boolean
   }
 }, res: any) => {
   const { body } = req
@@ -172,6 +175,7 @@ apiMock.post('/prod-api/mock_api_create', async(req: {
     ...database,
     list: list.concat([{
       ...body,
+      normal_response: typeof body.normal_response === 'string' ? body.normal_response : JSON.stringify(body.normal_response||{}),
       author: Math.random() > 0.5 ? '张三' : '李四',
       create_time: dayjs().format('YYYY-MM-DD HH:mm:ss'),
       id: list.length + 1
@@ -184,5 +188,50 @@ apiMock.post('/prod-api/mock_api_create', async(req: {
     msg: '新增接口成功'
   })
 } )
+
+apiMock.put('/:id', async (req: {
+  body: {
+    describe: string
+    method: string
+    url: string
+    exception_status: number
+    normal_response: any
+    status: boolean
+  },
+  params: {
+    id: string
+  }
+}, res: any) => {
+  const { body, params: { id } } = req
+  const database = require(databasePath)
+  const { list } = database
+
+  const currentItemIndex = list.findIndex((item: any)=>parseInt(item.id) === parseInt(id))
+  if ( currentItemIndex < 0 ) {
+    res.send({
+      code: -1,
+      results: null,
+      msg: '不存在的id'
+    })
+    return
+  }
+  
+  list[currentItemIndex] = {
+    ...list[currentItemIndex],
+    ...body,
+    normal_response: typeof body.normal_response === 'string' ? body.normal_response : JSON.stringify(body.normal_response || {})
+  }
+  
+  const assembledData = {
+    ...database,
+    list
+  }
+  await fs.writeFileSync(databasePath, JSON.stringify(assembledData))
+  res.send({
+    code: 0,
+    results: list[currentItemIndex],
+    msg: '更新数据成功'
+  })
+})
 
 export default apiMock
